3d4cbf21832ed2ef81663273128a54075ea98233
vaegasu <vaulaigasu@gmail.com>
2023-01-07 18:43:36 -0700

    print

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -9,5 +9,7 @@ class Program
 
         Console.Write("What is your last name? ");
         string last_name = Console.ReadLine();
+        
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
