ce4bc256611820f921786fa7015587c03d23f288
vaegasu <vaulaigasu@gmail.com>
2023-01-07 19:12:27 -0700

    lettergrade

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,7 +5,41 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("What is your grade percentage? ");
-        string grade = Console.ReadLine();
-        
+        string usergrade = Console.ReadLine();
+        int percent = int.Parse(usergrade);
+
+        string lettergrade = "";
+
+        if (percent >= 90)
+        {
+            lettergrade = "A";
+        }
+        else if (percent >= 80)
+        {
+            lettergrade = "B";
+        }
+        else if (percent >= 70)
+        {
+            lettergrade = "C";
+        }
+        else if (percent > 60)
+        {
+            lettergrade = "D";
+        }
+        else
+        {
+            lettergrade = "F";
+        } 
+
+        Console.WriteLine($"Your letter grade is: {lettergrade}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Yay. You passed! :D");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass. :(");
+        }
     }
 }
