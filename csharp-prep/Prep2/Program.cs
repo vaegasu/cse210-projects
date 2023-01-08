b49c4ab8e7f4622b158af333128de76bb84365a0
vaegasu <vaulaigasu@gmail.com>
2023-01-07 18:48:29 -0700

    grade

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        
     }
 }
