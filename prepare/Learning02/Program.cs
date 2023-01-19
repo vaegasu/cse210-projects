323317c071c7c21dfb2e289bc2ef5c2c4909adbe
vaegasu <vaulaigasu@gmail.com>
2023-01-18 22:39:13 -0700

    program

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -17,7 +17,7 @@ class Program
         job2._endYear = 2023;
 
         Resume myResume = new Resume();
-        myResume._name = "Allison Rose";
+        myResume._name = "Vae Gasu";
 
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
