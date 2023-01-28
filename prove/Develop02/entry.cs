2b49ea1c896b8d4cf5724ca6c58539f8ee1a96d5
vaegasu <vaulaigasu@gmail.com>
2023-01-28 16:46:24 -0700

    cs

--- prove/Develop02/entry.cs
+++ prove/Develop02/entry.cs
@@ -1,6 +1,6 @@
 using System;
 
-public class Entry {
+class Entry {
     public string _prompt();
 
     public string _date();
--- prove/Develop02/journal.cs
+++ prove/Develop02/journal.cs
@@ -1,6 +1,6 @@
 using System;
 
-public class Journal
+class Journal
 {
     
 }
