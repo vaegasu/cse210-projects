27da46de95c55b43f0678caaa54e086e5ca6fa2c
vaegasu <vaulaigasu@gmail.com>
2023-01-28 17:38:25 -0700

    cs

--- prove/Develop02/journal.cs
+++ prove/Develop02/journal.cs
@@ -1,6 +1,36 @@
 using System;
 public class Journal
 {
+    public string _entries;
+
+    public List<Entry> _entry = new List<Entry>();
+
+    public void AddingEntry() 
+    {
+        Console.WriteLine("1. Write");
+    }
+
+    public void DisplayJournal()
+    {
+        Console.WriteLine("2. Display");
+
+    }
+
+    public void LoadFile()
+    {
+        Console.WriteLine("3. Load");
+
+    }
+
+    public void SaveFile() 
+    {
+        Console.WriteLine("4. Save");
+    }
+
+    public void Quit()
+    {
+        Console.WriteLine("5. Quit");
+    }
 
 }
 
