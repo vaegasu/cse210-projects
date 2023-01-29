b0c463a2fe9fe01c393c4f49be36a604b65c9d09
vaegasu <vaulaigasu@gmail.com>
2023-01-28 19:17:26 -0700

    cs

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -6,6 +6,15 @@ class Program
     {
         Console.WriteLine("Hello there! :D");
         Console.WriteLine("Welcome to my Journal Program!");
+        Console.WriteLine("Please select one of the following choices:");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
+        Console.WriteLine("What would you like to do? ");
+        
+
 
     }
 }
--- prove/Develop02/journal.cs
+++ prove/Develop02/journal.cs
@@ -1,41 +1,9 @@
 using System;
 public class Journal
 {
-    public string journal_entries;
-    public string write = "1. Write";
-    public string display = "2. Display";
-    public string load = "3. Load";
-    public string save = "4. Save";
-    public string quit = "5. Quit";
+    public string entry;
 
     public List<Entry> _entry = new List<Entry>();
 
-    public void AddingEntry() 
-    {
-        Console.WriteLine(write);
-    }
-
-    public void DisplayJournal()
-    {
-        Console.WriteLine(display);
-
-    }
-
-    public void LoadFile()
-    {
-        Console.WriteLine(load);
-
-    }
-
-    public void SaveFile() 
-    {
-        Console.WriteLine(save);
-    }
-
-    public void Quit()
-    {
-        Console.WriteLine(quit);
-    }
 
 }
-
