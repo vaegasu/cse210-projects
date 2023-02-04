3cc969ad7b6a4123673337815b4f16ecd9301ffd
vaegasu <vaulaigasu@gmail.com>
2023-02-03 23:22:35 -0700

    cs

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,32 +5,29 @@ using System.IO;
 class Program
 {
     static void Main(string[] args)
-{
-    bool start = true;
+    {
+        bool start = true;
+        List<string> entries = new List<string>()
+    {   
+    };
 
-    while (start == true) {
-    while (start == true){
-        Menu menu1 = new Menu();
-        Console.WriteLine("Welcome to Vae's Journal Program.");
-        Console.WriteLine("Please choose from the following:")
-        Console.WriteLine("What would you like to do? ");
-        menu1.DisplayMenuOpts();
-        string userChoice = Console.ReadLine();
+        while (start == true){
+            Console.WriteLine("Welcome to Vae's Journal :D");
+            Console.WriteLine("Please choose from the following:");
+            Menu menu1 = new Menu();
+            Console.WriteLine("What would you like to do today? ");
+            menu1.DisplayMenuOpts();
+            string userChoice = Console.ReadLine();
 
     if (userChoice == "1") {
-        JEntry NewEntry = new JEntry();
-        string date = DateTime.UtcNow.ToString("MM/dd/yyyy"); 
-        string prompt = displayPrompt();
-        Console.WriteLine(prompt);
-        string words = Console.ReadLine();
-        string entry = (date + ", " + prompt + ", " + words);
-        entries.Add(entry);
-
-        Console.Write($"{prompt}\n>");
-        string text = Console.ReadLine();
-        Journal anEntry = new Journal(date, prompt, text);
-        entries.Add($"{date}, {prompt}, {text}");
-    }
+            string date = DateTime.UtcNow.ToString("MM/dd/yyyy"); 
+            string prompt = displayPrompt();
+            Console.Write($"{prompt}\n>");
+            string text = Console.ReadLine();
+            string anEntry = ($"{date}, {prompt}, {text}");
+            Journal entry1 = new Journal(date, prompt, text);
+            entries.Add($"{date}, {prompt}, {text}");
+        }
 
     else if (userChoice == "2") {
         foreach(string entry in entries){
@@ -42,7 +39,6 @@ class Program
     else if (userChoice == "3"){
         Console.Write("Enter a filename to load from, .txt: ");
         string filename = Console.ReadLine();
-        Console.Write(System.IO.File.ReadAllLines(filename));
         string[] lines = System.IO.File.ReadAllLines(filename);
         foreach(string line in lines)
         {
@@ -57,18 +53,41 @@ class Program
     else if (userChoice == "4"){
         Console.Write("Enter a filename to save to, .txt: ");
         string filename = Console.ReadLine();
-        foreach(string entry in entries){
-        System.IO.File.WriteAllText(filename, entry);
         foreach(string entry in entries)
         {
             System.IO.File.WriteAllText(filename, entry);
         };
     }
     else if (userChoice == "5") {
-        start = false;
-        Console.WriteLine("Thank you for stopping by, See you later. :D");
+        Console.WriteLine("Thank you for stopping by. See you later. :D");
         start = false;
     }
     }
 
 }
+
+}
+
+ static List<string> prompt = new List<string>()
+        {
+            "How did Heavenly Father blessed you today? (:",
+            "What are two things you accomplished today? (:",
+            "Share a favorite scripture of yours and explain why it's your favorite. (:",
+            "Share a time someone went out of there way for you. (:",
+            "What's on your mind today? And why? (:",
+            "What is something you can do to bless someone today? (:",
+            "What's something you would tell your past self? (:",
+            "Write a letter to someone in Heaven (:",
+            "Write a letter to someone you haven't seen a while",
+            "How was your day? What was the highlight of your day? Anything new?",
+
+        };
+
+    static string displayPrompt()
+    {
+        var random = new Random();
+        int index = random.Next(prompt.Count);
+        string randomPrompt = prompt[index];
+        return randomPrompt;
+
+    }       
--- prove/Develop02/entry.cs
+++ prove/Develop02/entry.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 
 public class Entry
  {   
-List<Entry> entries = new List<Entry>(){
+    List<Entry> entries = new List<Entry>()
+    {
         
     };
 
