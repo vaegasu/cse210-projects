6a9b6c1ccdfe0d049a4625977a3a25c9cb078c45
vaegasu <vaulaigasu@gmail.com>
2023-02-03 23:07:19 -0700

    cs

--- prove/Develop02/prompt.cs
+++ prove/Develop02/prompt.cs
@@ -1,13 +1,28 @@
-using System;
-public class Prompt
+
+public class Prompt; 
 {
-    public string _prompts = "";
+    static List<string> prompt = new List<string>()
+        {
+            "How did Heavenly Father blessed you today? (:",
+            "What are two things you accomplished today? (:",
+            "Share a favorite scripture of yours and explain why it's your favorite. (:",
+            "Share a time someone went out of there way for you. (:",
+            "What's on your mind today? And why? (:",
+            "What is something you can do to bless someone today? (:",
+            "What's something you would tell your past self? (:",
+            "Write a letter to someone in Heaven (:",
+            "Write a letter to someone you haven't seen a while",
+            "How was your day? What was the highlight of your day? Anything new?",
 
-    public List<Prompt> _prompt = new List<Prompt>();
+        };
 
-public void GeneratePrompt(){
-    Console.WriteLine(_prompts);
+    static string displayPrompt()
+    {
+        var random = new Random();
+        int index = random.Next(prompt.Count);
+        string randomPrompt = prompt[index];
+        return randomPrompt;
+
+    }
+
 }
-
-}
-
