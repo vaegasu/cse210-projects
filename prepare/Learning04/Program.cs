8cca77388f56df5cb9a06bc03f58024ddce8e232
vaegasu <vaulaigasu@gmail.com>
2023-02-15 22:20:21 -0700

    learning04 cs

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Base for assignment
+        Assignment a1 = new Assignment("Eirilani Aulai", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        // Derived class assignments
+        MathAssignment a2 = new MathAssignment("Eirike Maafala", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Christina Alama", "European History", "The Causes of World War II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
