7cab4772643f25eef949c7c2aa9f69329c03a2b9
vaegasu <vaulaigasu@gmail.com>
2023-03-25 22:26:53 -0600

    foundation 2

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -8,7 +8,7 @@ class Program
         List<Order> orders = new List<Order>();
 
         Order order1 = new Order();
-        Customer cust1 = new Customer("Sariah", 70700);
+        Customer cust1 = new Customer("Sarah", 70700);
         Address add1 = new Address("1234 Black St.", "Sheboygan","Michigan","12345","USA");
         Product prod1 = new Product(10234, "Black Boots size 9", 45.00, 1);
         Product prod2 = new Product(12345, "5 foot rope", 20.00, 5);
